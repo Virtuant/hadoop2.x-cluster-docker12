efffabc3ddacc0d381a541df9eca7a8d8b0dd774
James B. Matthews <james30152@gmail.com>
2016-12-14 15:07:12 -0500

    Update resize-cluster.sh

--- resize-cluster.sh
+++ resize-cluster.sh
@@ -23,6 +23,6 @@ echo ""
 echo -e "\nbuild docker hadoop image\n"
 
 # rebuild kiwenlau/hadoop image
-sudo docker build -t virtuant/hadoop:latest . #CHANGE THIS
+sudo docker build -t virtuant/hadoop-slave:latest . 
 
 echo ""
