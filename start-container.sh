a0b2cf20d44bfa519b829ff7057e7f2971fd4f23
James B. Matthews <james30152@gmail.com>
2016-12-14 15:06:31 -0500

    Update start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -9,13 +9,28 @@ sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
                 --net=hadoop \
+                -p 50090:50090 \
                 -p 50070:50070 \
+                -p 50470:50470 \
+                -p 10020:10020 \
+                -p 19888:19888 \
+                -p 9000:9000 \
                 -p 8088:8088 \
-		-p 8080:8080 \
-		-p 8020:8020 \
+                -p 8080:8080 \
+                -p 8000:8000 \
+                -p 8670 \
+                -p 8440 \
+                -p 8441 \
+                -p 8020 \
+                -p 3888 \
+                -p 3306 \
+                -p 3000 \
+                -p 2888 \
+                -p 2181 \
+                -p 22 \
                 --name hadoop-master \
                 --hostname hadoop-master \
-                viruant/hadoop:latest #CHANGE THIS
+                viruant/hadoop-master:latest
 
 
 # start hadoop slave container
@@ -26,11 +41,20 @@ do
 	echo "start hadoop-slave$i container..."
 	sudo docker run -itd \
 	                --net=hadoop \
-			-p 8440:8440 \
-			-p 8441:8441 \
+                    -p 50075 \
+                    -p 50475 \
+                    -p 50010 \
+                    -p 50020 \
+                    -p 8670 \
+                    -p 8440 \
+                    -p 8441 \
+                    -p 8020 \
+                    -p 3306 \
+                    -p 3000 \
+                    -p 22 \
 	                --name hadoop-slave$i \
 	                --hostname hadoop-slave$i \
-	                virtuant/hadoop:latest #CHANGE THIS
+	                virtuant/hadoop-slave:latest
 	i=$(( $i + 1 ))
 done 
 
